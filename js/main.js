d983405807bceda5aa4d3ec06f63d998c0176aac
yeeun <aappllee009@gmail.com>
2024-12-30 15:09:05 +0900

    탑버튼 수정 및 스크립트 작성

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// #top_btn 이동 -------------------------------------------
+const topBtn = document.querySelector('#top_btn')
+
+topBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+
+
+//  메인 슬라이더 ----------------------------------------------
 const swiper = new Swiper('.swiper', {
     loop: true,
     autoplay: {
--- js/sub.js
+++ js/sub.js
@@ -1,3 +1,15 @@
+// #top_btn 이동 -------------------------------------------
+const topBtn = document.querySelector('#top_btn')
+
+topBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+
+
+// 서브 메뉴 슬라이더  --------------------------------------
 const subMenu = new Swiper('.sub_inner_menu', {
     slidesPerView: 5,
     freeMode: true,
@@ -8,6 +20,7 @@ const subMenu = new Swiper('.sub_inner_menu', {
     },
 });
 
+// con3 슬라이더 ----------------------------------------------
 const con3Slider = new Swiper('#con3_wrap', {
     slidesPerView: 4,
     freeMode: true,
